0b6af383ea041519d88770ff84c953730f4b4570
Yash Bhawsar <30313851+yash872@users.noreply.github.com>
2021-01-26 15:09:06 +0530

    Create Find_the_Duplicate_Number.py

---
+++ Find_the_Duplicate_Number.py
@@ -0,0 +1,23 @@
+'''
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+'''
+
+
+def findDuplicate(self, nums: List[int]) -> int:
+    fast = slow = nums[0]
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if fast == slow:
+            break
+    slow = nums[0]
+    while fast!=slow:
+        slow = nums[slow]
+        fast = nums[fast]
+    return fast
