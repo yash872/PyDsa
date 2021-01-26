9d726f4961fac21bb5132dc2ec753a1ffbc497d4
Yash Bhawsar <30313851+yash872@users.noreply.github.com>
2021-01-26 18:49:20 +0530

    [KADANE's ALGORITHM]

--- Array/Maximum_Subarray.py
+++ Array/Maximum_Subarray.py
@@ -6,6 +6,10 @@ Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 '''
+#---------------------
+# KADANE's ALGORITHM
+#----------------------
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         s=0
