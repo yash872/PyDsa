a3af5fc184ae306ec7d119df142d19a53fd712a9
Yash Bhawsar <30313851+yash872@users.noreply.github.com>
2021-01-31 13:37:25 +0530

    Create Grid_Unique_Paths.py

---
+++ Array/Grid_Unique_Paths.py
@@ -0,0 +1,45 @@
+'''
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+'''
+#-------- Recursion + DP ------------------
+# Time-> O(2^m+n-2) | Space-> O(m x n)
+#------------------------------------------
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[-1 for i in range(n)] for j in range(m)]
+        i,j=0,0
+        def paths(i,j,m,n,dp):
+            #Base Case
+            if(i==(m-1) and j==(n-1)):
+                return 1
+            if(i>=m or j>=n):
+                return 0
+            
+            if(dp[i][j]!=-1):
+                return dp[i][j]
+            else:
+                dp[i][j]=paths(i+1,j,m,n,dp)+paths(i,j+1,m,n,dp)
+                return dp[i][j]
+        return paths(i,j,m,n,dp)
+        
+#-------- Combinatorics ------------------
+# Time-> O(row) or O(col) | Space-> O(1)
+#-----------------------------------------
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        N = m+n-2
+        r = m-1
+        res=1
+        # --- Calculate NCr ---
+        for i in range(1,r+1):
+            res = res*(N-r+i)/i
+        return int(res)
